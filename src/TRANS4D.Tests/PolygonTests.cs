81c4de7faf6b3a5f42e45586ed30b7279ec27fa4
Collin Tewalt <collin@sonicscholar.com>
2023-11-22 13:42:05 -0600

    Added Polygon class with the ability to check if it contains a given point (optionally include edges, but will be less performant since extra checks are required)

---
+++ src/TRANS4D.Tests/PolygonTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRANS4D.Geometry;
+
+namespace TRANS4D.Tests
+{
+    public class PolygonTests
+    {
+        public static List<(double X, double Y)> SimpleBox = new List<(double, double y)>
+        {
+            (0, 0),
+            (0, 3),
+            (3, 3),
+            (3, 0)
+        };
+
+        public static List<(double X, double Y)> SimpleStar = new List<(double x, double y)>
+        {
+            (0,4), //top
+            (1,0), (3,0), (1,-1), (3,-4), (0,-2), //right half
+            (-3,-4), (-1, -1), (-3,0), (-1,0), (0,4) //left half
+        };
+
+        [Theory]
+        [InlineData(2, 2, true)]  // Point inside the box
+        [InlineData(0, 1, true)]  // Point on edge of the box
+        [InlineData(0, 0, true)]  // Point on corner of the box
+        [InlineData(-1, -1, false)] // Point outside of the box
+        public void TestContainsPoint_OnSimpleBox(double x, double y, bool expected)
+        {
+            // Define a box with corners at (0,0), (0,3), (3,3), (3,0)
+            var vertices = SimpleBox;
+
+            var xPts = vertices.Select(v => v.X);
+            var yPts = vertices.Select(v => v.Y);
+
+            Polygon box = new Polygon(xPts, yPts);
+
+            bool result = box.ContainsPoint(x, y);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(0,0,false, true)]
+        [InlineData(0,0,true, true)]
+        [InlineData(4,0, true, false)]
+        [InlineData(4,0, false, false)]
+        [InlineData(1,-1, false, false)]
+        [InlineData(1,-1, true, true)]
+        [InlineData(2,0, false, false)]
+        [InlineData(2,0, true, true)]
+        [InlineData(0,4,false, false)]
+        [InlineData(0,4,true, true)]
+        public void TestContainsPoint_OnStarShape(double x, double y, bool includeEdges, bool expected)
+        {
+            var vertices = SimpleStar;
+
+            var xPts = vertices.Select(v => v.X);
+            var yPts = vertices.Select(v => v.Y);
+
+            Polygon star = new Polygon(xPts, yPts);
+
+            bool result = star.ContainsPoint(x, y, includeEdges);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Polygon_Constructor_InvalidArguments_Throws()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                _ =new Polygon(new[] { 0.0 }, new[] { 0.0, 1 })
+            );
+        }
+    }
+}
---
+++ src/TRANS4D/Geometry/Polygon.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace TRANS4D.Geometry
+{
+    public class Polygon
+    {
+        public List<(double X, double Y)> Vertices { get; private set; }
+        public double MinX { get; private set; }
+        public double MaxX { get; private set; }
+        public double MinY { get; private set; }
+        public double MaxY { get; private set; }
+        // Future: BoundingBox for R-Tree
+
+        public Polygon(IEnumerable<double> X, IEnumerable<double> Y)
+        {
+            double[] x = X.ToArray();
+            double[] y = Y.ToArray();
+            if (x.Length != y.Length)
+                throw new ArgumentException("X and Y must have the same number of elements");
+            Vertices = Enumerable.Range(0, x.Length)
+                .Select(i =>
+                {
+                    //update bounds as we go
+                    if (i == 0)
+                    {
+                        MinX = x[i];
+                        MaxX = x[i];
+                        MinY = y[i];
+                        MaxY = y[i];
+                    }
+                    else
+                    {
+                        MinX = Math.Min(MinX, x[i]);
+                        MaxX = Math.Max(MaxX, x[i]);
+                        MinY = Math.Min(MinY, y[i]);
+                        MaxY = Math.Max(MaxY, y[i]);
+                    }
+                    return (x[i], y[i]);
+                }).ToList();
+        }
+
+
+        public bool ContainsPoint(double x, double y, bool includeEdge = false)
+        {
+            bool inside = false;
+            int j = Vertices.Count - 1;
+
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                var (xi, yi) = Vertices[i];
+                var (xj, yj) = Vertices[j];
+
+                //check if point is on the edge (the segment between two vertices)
+                // given a point p1 and p2, chec
+                if(includeEdge && (x,y).IsPointOnLineSegment(xi, yi, xj, yj))
+                    return true;
+
+                if ((yi > y) != (yj > y) &&
+                    (x < (xj - xi) * (y - yi) / (yj - yi) + xi))
+                {
+                    inside = !inside;
+                }
+
+                j = i;
+            }
+
+            return inside;
+        }
+
+    }
+
+    internal static class TupleExtension
+    {
+        public static bool IsPointOnLineSegment(this (double x, double y) point,
+            double p1x, double p1y, double p2x, double p2y)
+        {
+            var (px, py) = point;
+            
+            //two approaches here if P1 and P2 are coincident
+            //always return false, since it's not really a segment?
+            //or say we can allow a segment of zero length, in which case
+            //we check if the test point is also equal to p1 & p2
+            //I opted for the latter since the goal of this function
+            //is to be used for checking if a test point is on an
+            //edge or corner of a polygon. This check may happen on
+            //a polygon whose last point == first point so that it's properly
+            //closed off.
+            if (p1x == p2x && p1y == p2y)
+                return  px == p1x && py == p1y;
+
+            // Check if p is collinear with p1 and p2
+            double crossProduct = (px - p1x) * (p2y - p1y) - (py - p1y) * (p2x - p1x);
+            if (Math.Abs(crossProduct) > float.Epsilon) return false;  // Not collinear
+
+            // Check if p is within the bounds of the segment
+            double dotProduct = (px - p1x) * (p2x - p1x) + (py - p1y) * (p2y - p1y);
+            if (dotProduct < 0) return false; // p is beyond p1
+
+            double squaredLengthP1P2 = (p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y);
+            if (dotProduct > squaredLengthP1P2) return false; // p is beyond p2
+
+            return true; // p is on the segment
+        }
+    }
+}
