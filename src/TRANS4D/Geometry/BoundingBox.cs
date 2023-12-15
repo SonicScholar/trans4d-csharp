382e9a65141113b1ac316e5dc49afb5d87782627
Collin Tewalt <collin@sonicscholar.com>
2023-12-15 15:22:05 -0700

    Added Data files and bounding box

---
+++ src/TRANS4D/Geometry/BoundingBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRANS4D.Geometry
+{
+    public class BoundingBox
+    {
+        public double XMin { get; set; }
+        public double XMax { get; set; }
+        public double YMin { get; set; }
+        public double YMax { get; set; }
+
+        public BoundingBox(double xMin, double xMax, double yMin, double yMax)
+        {
+            XMin = xMin;
+            XMax = xMax;
+            YMin = yMin;
+            YMax = yMax;
+        }
+    }
+}
